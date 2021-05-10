514feec48a4ba1e0cfa240efcdf6761b656e092c
Sakhib Sayfiev <sayfievsakhib00@gmail.com>
2021-05-11 03:55:34 +0500

    myToyList +- рабочий вариант односвязного списка небольшой прогресс с таблицей

--- Table.cpp
+++ Table.cpp
@@ -1,5 +1,164 @@
-//
-// Created by User on 01.05.2021.
-//
-
 #include "Table.h"
+
+
+bool operator== (const ArrayCell& left, const ArrayCell& right) {
+    if (left.key == right.key && left.key_size == right.key_size)
+        return true;
+
+    return false;
+}
+
+///////////////* ФУНКЦИИ КЛАССА ITERATOR *\\\\\\\\\\\\\\\
+
+void* Table::TableIterator::getElement(size_t &size) {
+    return (current_cell->getElement(size));
+}
+
+bool Table::TableIterator::hasNext() {
+    if(current_cell->hasNext())
+        return true;
+    else{
+        for (int i = index + 1; i < container_size; ++i) {
+            if(!table->map[i]->empty())
+                return true;
+        }
+    }
+    return false;
+}
+
+void Table::TableIterator::goToNext() {
+    bool flag = false;
+    if (current_cell->hasNext())
+        current_cell->goToNext();
+    else {
+        for (int i = index + 1; i < container_size; ++i) {
+            if (!table->map[i]->empty()) {
+                current_cell = table->map[i]->begin();
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            throw Error("There is no next one");
+        }
+    }
+}
+
+bool Table::TableIterator::equals(Iterator *right) {
+    if (current_cell == dynamic_cast<TableIterator*>(right)->current_cell)
+        return true;
+    return false;
+}
+
+///////////////* ПЕРСОНАЛЬНЫЕ ФУНКЦИИ КЛАССА TABLE *\\\\\\\\\\\\\\\
+
+int Table::insertByKey(void *key, size_t keySize, void *elem, size_t elemSize){
+    size_t index = hash_function(key, keySize);
+    ArrayCell temp = {key, keySize, elem, elemSize};
+    void* data = static_cast<void*>(&temp);
+    size_t data_size = sizeof data;
+    if(map[index]->find(data, data_size) == nullptr) {
+        map[index]->push_front(data, data_size);
+        elements_count++;
+        return 0;
+    }
+    return 1;
+}
+
+void Table::removeByKey(void *key, size_t keySize) {
+    size_t index = hash_function(key, keySize);
+    ArrayCell temp = {key, keySize, nullptr, 0};
+    void* data = static_cast<void*>(&temp);
+    size_t data_size = sizeof data;
+    List::Iterator *tmp = map[index]->find(data, data_size);
+    if(tmp != nullptr) {
+        map[index]->remove(tmp);
+        elements_count--;
+    }
+}
+
+Container::Iterator* Table::findByKey(void *key, size_t keySize){
+    size_t index = hash_function(key, keySize);
+    ArrayCell temp = {key, keySize, nullptr, 0};
+    void* data = static_cast<void*>(&temp);
+    size_t data_size = sizeof data;
+    return map[index]->find(data, data_size);
+}
+
+///// головная боль № 1\\\\\
+
+void* Table::at(void *key, size_t keySize, size_t &valueSize){
+    size_t index = hash_function(key, keySize);
+    ArrayCell temp = {key, keySize, nullptr, 0};
+    void* data = static_cast<void*>(&temp);
+    size_t data_size = sizeof data;
+    ArrayCell* findedCell = static_cast<ArrayCell*>(map[index]->find(data, data_size)->getElement(valueSize));
+    return findedCell->elem;
+}
+
+
+size_t Table::hash_function(void *key, size_t keySize){
+    size_t hash = 0;
+    char* why_not = static_cast<char*>(key);
+    for(int i = 0; i < keySize; ++i)
+        hash = 31 * hash + why_not[i];
+    return hash % 1'000'000;
+}
+
+///////////////* ФУНКЦИИ КЛАССА TABLE *\\\\\\\\\\\\\\\
+
+int Table::size() {
+    return elements_count;
+}
+
+size_t Table::max_bytes() {
+    return _memory.maxBytes();
+}
+
+///// головная боль №2 \\\\\
+
+//Container::Iterator* Table::find(void *elem, size_t size) {
+//    TableIterator* finder = newIterator();
+//    for (int i = 0; i < container_size; ++i) {
+//
+//    }
+//}
+
+Container::Iterator* Table::newIterator() {
+    TableIterator* iterator = new TableIterator(this, nullptr, 0);
+    return iterator;
+}
+
+Container::Iterator* Table::begin(){
+    if(empty())
+        return nullptr;
+    for (int i = 0; i < container_size; ++i) {
+        if(!this->map[i]->empty())
+            return new TableIterator(this, dynamic_cast<List::Iterator*>(this->map[i]->begin()), i);
+    }
+
+}
+
+Container::Iterator* Table::end(){
+    if(empty())
+        return nullptr;
+    for (int i = container_size - 1; i >= 0; ++i) {
+        if(!this->map[i]->empty())
+            return new TableIterator(this, dynamic_cast<List::Iterator*>(this->map[i]->end()), i);
+    }
+}
+
+void Table::remove(Iterator *iter) {
+
+}
+
+void Table::clear() {
+
+}
+
+bool Table::empty() {
+    if(elements_count == 0)
+        true;
+    return false;
+}
+
--- Table.h
+++ Table.h
@@ -1,16 +1,101 @@
-//
-// Created by User on 01.05.2021.
-//
-
 #ifndef MSU_CONTAINERLIBRARY_TABLE_H
 #define MSU_CONTAINERLIBRARY_TABLE_H
+#include "TableAbstract.h"
+#include "myToyList.h"
+#include "container.h"
 
-
-
-class Table {
-
+struct ArrayCell{
+    void* key;
+    size_t key_size;
+    void* elem;
+    size_t elem_size;
 };
 
+class Table : public AbstractTable {
+private:
+    static const size_t container_size = 1'000'000;
+    List *map[1'000'000];
+    size_t elements_count = 0;
 
+public:
+
+    Table(MemoryManager &mem): AbstractTable(mem) {}
+
+    class TableIterator : public Container::Iterator  {
+    public:
+        Table *table;
+        List::Iterator *current_cell;
+        int index;
+
+        TableIterator(Table *table, List::Iterator *current_cell, int index):
+                table(table),
+                current_cell(current_cell),
+                index(index) {}
+
+        void *getElement(size_t &size) override;
+
+        // Возвращает true, если есть следующий элемент, иначе false.
+        bool hasNext() override;
+
+        // Переходит к следующему элементу.
+        void goToNext() override;
+
+        // Возвращает true, если текущий итератор равен заданному, иначе false.
+        bool equals(Iterator *right) override;
+
+        ~TableIterator() = default;
+
+        friend Table;
+    };
+
+    // Добавление элемента в контейнер, с сответствующим ключом.
+    // В случае успешного добавления функция возвращает значение 0, в случае неудачи 1.
+    int insertByKey(void *key, size_t keySize, void *elem, size_t elemSize) override;
+
+    // Удаление элемента с сответствующим ключом из контейнера.
+    void removeByKey(void *key, size_t keySize) override;
+
+    // Функция возвращает указатель на итератор, указывающий на найденный в контейнере элемент с сответствующим ключом.
+    // Если элемент не найден, возвращается нулевой указатель.
+    Iterator *findByKey(void *key, size_t keySize) override;
+
+    // доступ к элементу с ключом key
+    void *at(void *key, size_t keySize, size_t &valueSize) override;
+
+    // хэш функция
+    size_t hash_function(void *key, size_t keySize) override;
+
+    // Возвращает значение, равное количеству элементов в контейнере.
+    int size() override;
+
+    // Возвращает значение, равное максимальной вместимости контейнера в байтах.
+    size_t max_bytes() override;
+
+    // Возвращает указатель на итератор, указывающий на первый найденный в контейнере элемент.
+    // Если элемент не найден, возвращает пустой указатель.
+    Iterator *find(void *elem, size_t size) override;
+
+    // Создает итератор, соответствующий данному типу контейнера.
+    Iterator *newIterator() override;
+
+    // Возвращает указатель на итератор, указывающий на первый элемент контейнера.
+    // Если контейнер пустой, возвращает нулевой указатель.
+    Iterator *begin() override;
+
+    // Возвращает указатель на итератор, указывающий позицию за последним элементом контейнера.
+    // Если контейнер пустой, возвращает нулевой указатель.
+    Iterator *end() override;
+
+    // Удаляет элемент из позиции, на которую указывает итератор iter.
+    // После удаления итератор указывает на следующий за удаленным элемент.
+    void remove(Iterator *iter) override;
+
+    // Удаляет все элементы из контейнера.
+    void clear() override;
+
+    // Если контейнер пуст возвращает true, иначе false.
+    bool empty() override;
+
+};
 
 #endif //MSU_CONTAINERLIBRARY_TABLE_H
