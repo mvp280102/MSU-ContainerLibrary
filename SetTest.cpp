a516c7c28f0910464b41c70a9e5b726faf435f7c
Maxim Pochinskiy <mvp280102@gmail.com>
2021-06-15 09:16:09 +0500

    Tests added.

---
+++ SetTest.cpp
@@ -0,0 +1,62 @@
+#include "SetTest.h"
+
+// Тестирует insert() и find().
+// Добавляет 1'000'000 элементов с помощью insert()
+// и проверяет их фактическое наличие с помощью find().
+// В случае успеха возвращает 0, в случае неудачи - 1.
+// В случае неудачи выводит номер элемента, вызвашего ошибку.
+int SetTest::insert_find_test()
+{
+	Set::SetIterator *set_iterator;
+
+	for (int i = 0; i < CONTAINER_SIZE; i++)
+	{
+		set->insert(&i, sizeof(i));
+
+		set_iterator = dynamic_cast<Set::SetIterator*>(set->find(&i, sizeof(i)));
+
+		if (set_iterator == nullptr)
+		{
+			cout << "Element " << i << " not found!" << endl;
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+// Тестирует remove() и find().
+// Удаляет все нечетные элементы с помощью remove()
+// и проверяет наличие четных и отсутствие нечетных с помощью find().
+// В случае успеха возвращает 0, в случае неудачи - 1.
+// В случае неудачи выводит номер элемента, вызвашего ошибку.
+int SetTest::remove_find_test()
+{
+	Set::SetIterator *set_iterator;
+
+	for (int i = 0; i < CONTAINER_SIZE; i++)
+	{
+		set_iterator = dynamic_cast<Set::SetIterator*>(set->find(&i, sizeof(i)));
+
+		if (i & 1)
+		{
+			set->remove(set_iterator);
+
+			set_iterator = dynamic_cast<Set::SetIterator*>(set->find(&i, sizeof(i)));
+
+			if (set_iterator != nullptr)
+			{
+				cout << "Uneven element " << i << " found!" << endl;
+				return 1;
+			}
+		}
+		else
+		if (set_iterator == nullptr)
+		{
+			cout << "Even element " << i << " not found!" << endl;
+			return 1;
+		}
+	}
+
+	return 0;
+}
